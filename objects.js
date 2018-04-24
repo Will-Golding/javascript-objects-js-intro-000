a9c00fb1db9a5d4920bf9eacf9f8b73d43dcc701
will Golding <willgolding117@gmail.com>
2018-04-24 15:12:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,8 +3,9 @@ var playlist = { artistName:"AC/DC", songTitle: "Hell's Bells"};
 function updatePlaylist(playlist,artistName,songTitle){
   return playlist.assign({},artistName, songTitle)
 }
-
+/*
 function removeFromPlaylist(playlist, artistName){
   delete playlist.
   return playlist
 }
+*/
